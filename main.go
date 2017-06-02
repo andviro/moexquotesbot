704086d1c8763e1856a201802333d1fe9cf5460d
Maxim <mvaleev@users.noreply.github.com>
2017-06-02 18:24:09 +0300

    22

--- main.go
+++ main.go
@@ -1,32 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/spf13/viper"
 	"gopkg.in/telegram-bot-api.v4"
 )
 
 type (
-	MsgResponse struct {
-		chatId    int64
-		messageId int
+	msgResponse struct {
+		chatID    int64
+		messageID int
 		text      string
 	}
-	MsgRequest struct {
-		chatId    int64
-		messageId int
+	msgRequest struct {
+		chatID    int64
+		messageID int
 		text      string
+	}
+	issRespJSON struct {
+		Securities struct {
+			Columns []string   `json:"columns"`
+			Data    [][]string `json:"data"`
+		} `json:"securities"`
 	}
 )
 
 var (
+	myClient           = &http.Client{Timeout: 2 * time.Second}
+	issURLSearchJSON   = "https://iss.moex.com/iss/securities.json?q="
+	issURLSearchParams = "&iss.only=marketdata&iss.meta=off&securities.columns=secid,name"
+	issURLImg          = "https://iss.moex.com/cs/engines/stock/markets/index/securities/"
+
 	configFile string
-	chnResp    = make(chan MsgResponse, 5)
-	chnReq     = make(chan MsgRequest, 5)
+	chnResp    = make(chan msgResponse, 5)
+	chnReq     = make(chan msgRequest, 5)
 )
 
 func main() {
@@ -45,9 +59,9 @@ func main() {
 
 	go getResp()
 
-	botApiKey := viper.GetString("api_key")
+	botAPIKey := viper.GetString("api_key")
 
-	bot, err := tgbotapi.NewBotAPI(botApiKey)
+	bot, err := tgbotapi.NewBotAPI(botAPIKey)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -59,20 +73,63 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	updates, err := bot.GetUpdatesChan(u)
+	updates, _ := bot.GetUpdatesChan(u)
 
 	for {
 		select {
 		case update := <-updates:
-			chatID := update.Message.Chat.ID
-			msgID := update.Message.MessageID
-			msgText := update.Message.Text
 
-			chnReq <- MsgRequest{chatID, msgID, msgText}
+			if update.Message != nil && !(update.Message.Chat.IsGroup() || update.Message.Chat.IsSuperGroup()) {
+				chatID := update.Message.Chat.ID
+				msgID := update.Message.MessageID
+				msgText := update.Message.Text
+
+				chnReq <- msgRequest{chatID, msgID, msgText}
+			}
+
+			if update.Message == nil && update.InlineQuery != nil {
+				var articles []interface{}
+				issResponse := issRespJSON{}
+
+				query := update.InlineQuery.Query
+				getJSON(issURLSearchJSON+query+issURLSearchParams, &issResponse)
+
+				fmt.Println(issResponse)
+
+				if len(issResponse.Securities.Data) == 0 {
+					msg := tgbotapi.NewInlineQueryResultArticle(update.InlineQuery.ID, "No one index matches", "No one index matches")
+					articles = append(articles, msg)
+				} else {
+					for i, indx := range issResponse.Securities.Data {
+						text := fmt.Sprintf("*%s*"+
+							"[.](%s)\n",
+							indx[0]+" - "+indx[1],
+							issURLImg+indx[0])
+
+						msg := tgbotapi.NewInlineQueryResultArticleMarkdown(indx[0], indx[0]+" - "+indx[1], text)
+						articles = append(articles, msg)
+						if i >= 6 {
+							break
+						}
+					}
+				}
+
+				inlineConfig := tgbotapi.InlineConfig{
+					InlineQueryID: update.InlineQuery.ID,
+					IsPersonal:    true,
+					CacheTime:     0,
+					Results:       articles,
+				}
+
+				_, err := bot.AnswerInlineQuery(inlineConfig)
+				if err != nil {
+					log.Println(err)
+				}
+			}
 
 		case resp := <-chnResp:
-			msg := tgbotapi.NewMessage(resp.chatId, resp.text)
-			msg.ReplyToMessageID = resp.messageId
+			msg := tgbotapi.NewMessage(resp.chatID, resp.text)
+			msg.ReplyToMessageID = resp.messageID
 			bot.Send(msg)
 		}
 	}
@@ -81,6 +138,16 @@ func main() {
 func getResp() {
 	// goroutine for magic
 	for msg := range chnReq {
-		chnResp <- MsgResponse{msg.chatId, msg.messageId, msg.text}
+		chnResp <- msgResponse{msg.chatID, msg.messageID, msg.text}
 	}
+}
+
+func getJSON(url string, target interface{}) error {
+	r, err := myClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
 }
